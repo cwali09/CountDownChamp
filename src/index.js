29c348f428c2e3e945e6fc6f2f29e27a737a48ba
Wali Chaudhary <cwali0914@gmail.com>
2017-04-21 22:46:26 -0500

    Add App.jsx Component and create CSS classes

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; //allows you to throw code onto the browser
+import App from './App.jsx'
+
+ReactDOM.render(
+  <App />, //grabs the root id in the index.html file
+  document.getElementById('root')
+
+);
