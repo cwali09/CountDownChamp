3c4485292d055d32112c3aa3ca675a4b93c87422
Wali Chaudhary <cwali0914@gmail.com>
2017-04-24 19:55:56 -0500

    Add ability for state updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,26 @@ import './App.css';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2017',
+      newDeadline: '' //allows you to update state
+    }
+  }
+
+  changeDeadLine() {
+    this.setState({deadline: this.state.newDeadline});
+  }
+
+
+
+
   render() {
     return (
       <div className="App">
 
-        <div className="App-title">Countdown to December 25, 2017</div>
+        <div className="App-title">{this.state.deadline}</div>
         <div>
           <div className="Clock-days">14 Days</div>
           <div className="Clock-hours">30 hours</div>
@@ -15,13 +30,17 @@ class App extends Component {
           <div className="Clock-seconds">20 seconds</div>
         </div>
         <div>
-          <input placeholder='new date'/>
-          <button>Submit</button>
+          <input
+            placeholder='new date'
+            onChange={event => this.setState({newDeadline: event.target.value})} //gives us what the user types
+          />
+        <button onClick={() => this.changeDeadLine()}>Submit</button>
         </div>
 
     </div>
     )
   }
 }
+//Having an anonymous function prevents loops and potential crashing of the app
 
 export default App;
